fda0fb483f0806f506ab83c76f7e771ae38bc3f8
Choi JaeYoung <chlwowudd0327@naver.com>
2022-09-29 16:47:25 +0900

    ProductDao 완료

--- src/test/java/site/metacoding/firstapp/util/MyParseTest.java
+++ src/test/java/site/metacoding/firstapp/util/MyParseTest.java
@@ -8,12 +8,18 @@ public class MyParseTest {
     @Test
     public void changeStringToInt_test() {
         //given
-        String value = "1";
+        // String value = "1";
 
         //when 
-        int result = Integer.parseInt(value);
+        // int result = Integer.parseInt(value);
 
         //then
-        assertEquals(1, result);
+        // assertEquals(1, result);
+
+        int a =1;
+        int b= 3;
+        int result = a+b;
+
+        assertEquals(4, result);
     }
 }
